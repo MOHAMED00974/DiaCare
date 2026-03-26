4ba32ebfd6b9d72a79d0a25b995bee877ea3edd6
Alia Harb <aliaharb11@outlook.com>
2026-03-26 04:29:31 +0200

    feat: implement User Profile service and controller

---
+++ DiaCare.WebAPI/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiaCare.WebAPI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
